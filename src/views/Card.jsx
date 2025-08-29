502b63d0cdbea9dda8dd4443c84323efe935b895
KrushnaBarhate03 <krushnabarhate822@gmail.com>
2025-08-29 19:21:52 +0530

    card added

---
+++ src/views/Card.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Mern from '../assets/mern-stack.png'
+
+function Card({name,description ,images}) {
+  return (
+
+    <div className="bg-white   shadow-xl/30  w-80 md:ml-20 ml-5 h-110 mt-15 border-t-4 border-blue-500 rounded-xl">
+
+            <img src={images} className="w-60 m-auto mt-8 "/>
+
+            <h4 className="text-blue-500 font-serif font-bold text-center mt-4 text-2xl">{name} </h4>
+
+            <h5 className='font-serif text-center mt-5'>{description}</h5>
+            </div> 
+    
+  )
+}
+
+export default Card
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,30 +1,50 @@
 import React from 'react'
-import Logo from '../assets/logo.png';
+import Card from './Card.jsx'
+import Mern from '../assets/mern-stack.png'
+
 
 function Home() {
   return (
     <>
     <div className="flex flex-col items-center mt-10">
+
       <h3 className="text-orange-500 font-serif font-bold text-4xl md:text-5xl"> Spaceship</h3> 
 
       <p className="text-orange-500 font-serif font-bold text-center px-14 text-1xl md:text-xl mt-5">Spaceship is a mini Wikipedia 📖 for computer science students, created by RoadToCode.</p>
-     
+
      <button className="md:mt-7 mt-8">
         <a href='#GetStarted' className="bg-orange-500 font-serif  text-white font-semibold p-3 rounded-lg ">Get Started</a>
      </button>
 
      <h6 className="mt-10 text-black font-bold font-serif  text-2xl md:text-4xl">Our Top Courses</h6>
-    
-    </div>
-    <div className="bg-amber-200 w-50 ml-20 md:ml-20 h-50">
-        <img src={Logo}/>
 
     </div>
-    <div>
 
-    </div>
-    </>
-    
+<div className='flex flex-row flex-wrap'>
+
+     <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+      
+      
+       <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+
+        <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+
+         <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+
+          <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+          
+           <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+
+            <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+
+             <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+
+              <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+      
+
+
+      </div>
+</>
   )
 }
 
