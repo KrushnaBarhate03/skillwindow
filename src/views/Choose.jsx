567140e88896e95a1bfa6c42237d1e3aba92045d
KrushnaBarhate03 <krushnabarhate822@gmail.com>
2025-08-29 21:32:23 +0530

    cardss added

---
+++ src/views/Choose.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Choose({name,description,images}) {
+  return (
+   <div className="bg-white   shadow-xl/30  w-80 md:ml-10  h-50 mt-15 border-t-4 border-blue-500 rounded-xl">
+
+            <img src={images} className="w-8 m-auto mt-8 "/>
+
+            <h4 className="text-blue-500 font-serif font-bold text-center mt-4 text-1xl">{name} </h4>
+
+            <h5 className='font-serif text-center mt-5'>{description}</h5>
+            </div> 
+    
+  )
+}
+
+export default Choose
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Card from './Card.jsx'
 import Mern from '../assets/mern-stack.png'
+import Mean from '../assets/mean.jfif';
+import AI from '../assets/AI.png';
+import Choose from './Choose.jsx';
+
 
 
 function Home() {
@@ -20,30 +24,22 @@ function Home() {
 
     </div>
 
-<div className='flex flex-row flex-wrap'>
+<div className='flex flex-row justify-evenly flex-wrap'>
 
      <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
       
       
-       <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+       <Card name="Mean Stack" images={Mean} description="The MEAN stack is a popular full-stack development framework used to build dynamic web applications."/>
 
-        <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
+        <Card name="Artificial Intelligence" images={AI} description="(AI) refers to machines designed to mimic human intelligence to perform tasks like learning, problem-solving, and decision-making."/>      
+</div>
+<h3 className="mt-20 text-center font-bold font-serif text-2xl">Why Choose US ?</h3>
 
-         <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
-
-          <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
-          
-           <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
-
-            <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
-
-             <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
-
-              <Card name="Mern Stack" images={Mern} description="The MERN stack is a popular set of technologies used for building modern web applications."/>
-      
-
-
-      </div>
+<div className="flex flex-row flex-wrap items-center"> 
+  <Choose name="Expert Faculty" images={Mern} description="Learn from top educators with years of experience."/>
+  <Choose name="Comprehensive Study Materials" images={Mern} description="Access high-quality notes, quizzes,and mock tests."/>
+    <Choose name="24/7 Doubt Support" images={Mern} description="Get your queries resolved instantly by experts."/>
+</div>
 </>
   )
 }
